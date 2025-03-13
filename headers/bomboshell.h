e8bdc8313de31d2291d5cce71621e9bdf6b3cf7f
Noe Martin <nmartin@bess-f4r3s14.clusters.42paris.fr>
2025-03-13 20:31:54 +0100

    bomboshell.h

---
+++ headers/bomboshell.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bomboshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmartin <nmartin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 19:10:53 by nmartin           #+#    #+#             */
+/*   Updated: 2025/03/13 19:22:46 by nmartin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BOMBOSHELL_H
+# define BOMBOSHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <signal.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <string.h>
+
+
+
+
+
+
+
+#endif
