36c59672d7b35d22bd9b61160fb27dd98696269c
Noe Martin <nmartin@bess-f4r4s13.clusters.42paris.fr>
2025-04-10 15:23:20 +0200

    export bug + output

--- built-ins/built_export.c
+++ built-ins/built_export.c
@@ -6,7 +6,7 @@
 /*   By: nmartin <nmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 19:37:52 by atazzit           #+#    #+#             */
-/*   Updated: 2025/04/06 16:27:36 by nmartin          ###   ########.fr       */
+/*   Updated: 2025/04/10 15:22:59 by nmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ static void	print_exports(t_env *env)
 int	ft_export(t_shell *cmd)
 {
 	int	i;
-
+	printf("\n");
 	if (!cmd->command[1])
 	{
 		print_exports(cmd->env_vars);
